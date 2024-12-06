64e64716592027a9923a566b9a828e846ec1edbc
Sanidhya14321 <sanidhya14321@gmail.com>
2024-12-06 22:56:57 +0530

    commit 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
-module.exports = {
+export default {
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./src//*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
         'bebas-neue': ['"Bebas Neue"', 'serif'],
         'poppins': ['"Poppins"', 'sans-serif'],
-      }
+      },
     },
   },
-  plugins: [],
+  plugins: [],
 };
