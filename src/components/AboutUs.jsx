8e16a23329632790a93df1181370888c6d8e3f01
amansangwan1 <codingkarle123@gmail.com>
2024-12-26 22:36:35 +0530

    css to tailwind(aboutUs)

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -18,11 +18,11 @@ const AboutUs = ({ id, className }) => {
   return (
     <section id={id} className={className}>
       {isMobile ? (
-        <div className="mobileView">
-          <div className="aboutUsTextBoxMobile">
+        <div className="flex flex-col justify-center items-center">
+          <div className="aboutUsTextBoxMobile text-[#272727] text-[6rem] font-extrabold whitespace-nowrap flex justify-center">
             <h1>ABOUT US</h1>
           </div>
-          <div className="carouselBox">
+          <div className="max-w-[500px] max-h-[380px] w-auto object-fill overflow-hidden">
             <div
               id="carouselExampleAutoplaying"
               className="carousel slide"
@@ -85,57 +85,56 @@ const AboutUs = ({ id, className }) => {
               </button>
             </div>
           </div>
-          <div className="textUnderAboutUsMobile">
-              <h1>
-                At GDSC MSIT,we aim to learn, teach and grow. Together, the
-                place where creative minds come together to build something
-                amazing
-              </h1>
+          <div className="flex justify-center text-[1.1rem] max-w-[500px] text-center mt-5">
+            <h1>
+              At GDSC MSIT,we aim to learn, teach and grow. Together, the place
+              where creative minds come together to build something amazing
+            </h1>
           </div>
         </div>
       ) : (
-        <div className="desktopView">
-          <div className="box box1">
+        <div className="relative m-[10px] flex justify-center items-center">
+          <div className="box1 z-100 bg-[#4285F4] absolute h-[250px] w-[200px] rounded-[20px] flex justify-center items-center overflow-hidden ">
             <img
               className="h-full w-auto object-cover"
               src="/src/assets/aboutUsImages/i4.jpg"
               alt=""
             />
           </div>
-          <div className="box box2">
+          <div className="box2 z-200 bg-[#FBBC04] absolute h-[250px] w-[200px] rounded-[20px] flex justify-center items-center overflow-hidden">
             <img src="/src/assets/aboutUsImages/i5.jpg" alt="" />
           </div>
-          <div className="box box3">
+          <div className="box3 z-300 bg-[#EA4335] absolute h-[250px] w-[200px] rounded-[20px] flex justify-center items-center overflow-hidden">
             <img
               className="h-full w-auto object-cover"
               src="/src/assets/aboutUsImages/i3.jpg"
               alt=""
             />
           </div>
-          <div className="box box4">
-            <img src="/src/assets/gdgLogo.png" alt="" />
+          <div className="box4 z-400 bg-[#1d1d1d] absolute h-[250px] w-[200px] rounded-[20px] flex justify-center items-center overflow-hidden">
+            <img src="/public/assets/gdgLogo.png" alt="" />
           </div>
-          <div className="box box5">
+          <div className="box5 z-500 bg-[#4285F4] absolute h-[250px] w-[200px] rounded-[20px] flex justify-center items-center overflow-hidden">
             <img src="/src/assets/aboutUsImages/i1.png" alt="" />
           </div>
-          <div className="box box6">
+          <div className="box6 z-600 bg-[#FBBC04] absolute h-[250px] w-[200px] rounded-[20px] flex justify-center items-center overflow-hidden">
             <img
               className="h-full w-auto object-cover"
               src="/src/assets/aboutUsImages/i7.jpg"
               alt=""
             />
           </div>
-          <div className="box box7">
+          <div className="box7 z-700 bg-[#EA4335] absolute h-[250px] w-[200px] rounded-[20px] flex justify-center items-center overflow-hidden">
             <img
               className="h-full w-auto object-cover"
               src="/src/assets/aboutUsImages/i2.jpg"
               alt=""
             />
           </div>
-          <div className="aboutUsTextBox">
+          <div className="aboutUsTextBox z-800 text-[#272727] absolute whitespace-nowrap">
             <h1>ABOUT US</h1>
           </div>
-          <div className="textUnderAboutUs">
+          <div className="textUnderAboutUs absolute text-[1.5rem] w-[600px] transform translate-x-[-400px] translate-y-[300px] text-center">
             <h1>
               At GDSC MSIT,we aim to learn, teach and grow. Together, the place
               where creative minds come together to build something amazing
