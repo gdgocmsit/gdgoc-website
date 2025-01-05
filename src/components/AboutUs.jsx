7e320cd0b4eaba5afc04a96bcf779fa15ded3a33
amansangwan1 <codingkarle123@gmail.com>
2025-01-05 13:08:29 +0530

    final bugfix try 2

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,20 +1,37 @@
-import React,{useState,useEffect} from 'react';
-import '../styles/AboutUs.css'
+import React, { useState, useEffect } from "react";
+import "../styles/AboutUs.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import AboutUs1 from "/public/assets/aboutUs1.png";
+import AboutUs2 from "/public/assets/aboutUs2.jpg";
+import AboutUs3 from "/public/assets/aboutUs3.jpg";
+import AboutUs4 from "/public/assets/aboutUs4.jpg";
+import AboutUs5 from "/public/assets/aboutUs5.jpg";
+import AboutUs7 from "/public/assets/aboutUs7.jpg";
 
 const AboutUs = ({ id, className }) => {
+  const [isMobile, setIsMobile] = useState(false);
 
-  const [isMobile,setIsMobile]=useState(false)
-
-  useEffect(()=>{
+  useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768)
-    }
+      setIsMobile(window.innerWidth <= 768);
+    };
 
-    handleResize()
-    window.addEventListener('resize',handleResize)
-    return () => window.removeEventListener('resize',handleResize)
-  },[])
-  
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const settings = {
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 4000,
+    cssEase: "linear",
+  };
   return (
     <section id={id} className={className}>
       {isMobile ? (
@@ -22,69 +39,29 @@ const AboutUs = ({ id, className }) => {
           <div className="aboutUsTextBoxMobile text-[#272727] text-[6rem] font-extrabold whitespace-nowrap flex justify-center">
             <h1>ABOUT US</h1>
           </div>
-          <div className="max-w-[500px] max-h-[380px] w-auto object-fill overflow-hidden">
-            <div
-              id="carouselExampleAutoplaying"
-              className="carousel slide"
-              data-bs-ride="carousel"
-              data-bs-interval="3000"
-            >
-              <div className="carousel-inner overflow-visible ">
-                <div className="carousel-item active">
-                  <img
-                    src="/assets/aboutUs4.jpg"
-                    className="d-block h-100vh scale-150"
-                    alt="..."
-                  />
-                </div>
-                <div className="carousel-item">
-                  <img
-                    src="/assets/aboutUs3.jpg"
-                    className="d-block h-100vh"
-                    alt="..."
-                  />
-                </div>
-                <div className="carousel-item">
-                  <img
-                    src="/assets/aboutUs2.jpg"
-                    className="d-block h-100vh"
-                    alt="..."
-                  />
-                </div>
-                <div className="carousel-item">
-                  <img
-                    src="/assets/aboutUs7.jpg"
-                    className="d-block h-100vh"
-                    alt="..."
-                  />
-                </div>
+          <div
+            style={{
+              maxHeight: "300px",
+              maxWidth: "550px",
+              overflow: "hidden",
+            }}
+          >
+            <Slider {...settings}>
+              <div>
+                <img src={AboutUs2} alt="" />
               </div>
-              <button
-                className="carousel-control-prev opacity-0"
-                type="button"
-                data-bs-target="#carouselExampleAutoplaying"
-                data-bs-slide="prev"
-              >
-                <span
-                  className="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
-                <span className="visually-hidden">Previous</span>
-              </button>
-              <button
-                className="carousel-control-next opacity-0"
-                type="button"
-                data-bs-target="#carouselExampleAutoplaying"
-                data-bs-slide="next"
-              >
-                <span
-                  className="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
-                <span className="visually-hidden">Next</span>
-              </button>
-            </div>
+              <div>
+                <img src={AboutUs3} alt="" />
+              </div>
+              <div>
+                <img src={AboutUs4} alt="" />
+              </div>
+              <div>
+                <img src={AboutUs7} alt="" />
+              </div>
+            </Slider>
           </div>
+
           <div className="flex justify-center text-[1.1rem] max-w-[500px] text-center mt-5">
             <h1>
               At GDSC MSIT,we aim to learn, teach and grow. Together, the place
@@ -94,47 +71,31 @@ const AboutUs = ({ id, className }) => {
         </div>
       ) : (
         <div className="relative m-[10px] flex justify-center items-center">
-          <div className="box1 z-100 bg-[#4285F4] absolute h-[220px] w-[176px] rounded-[20px] flex justify-center items-center overflow-hidden ">
-            <img
-              className="h-full w-auto object-cover"
-              src="/assets/aboutUs4.jpg"
-              alt=""
-            />
+          <div className="box1 z-100 bg-[#4285F4] absolute h-[250px] w-[200px] rounded-[20px] flex justify-center items-center overflow-hidden ">
+            <img className="h-full w-auto object-cover" src={AboutUs4} alt="" />
           </div>
-          <div className="box2 z-200 bg-[#FBBC04] absolute h-[220px] w-[176px] rounded-[20px] flex justify-center items-center overflow-hidden">
-            <img src="/assets/aboutUs5.jpg" alt="" />
+          <div className="box2 z-200 bg-[#FBBC04] absolute h-[250px] w-[200px] rounded-[20px] flex justify-center items-center overflow-hidden">
+            <img src={AboutUs5} alt="" />
           </div>
-          <div className="box3 z-300 bg-[#EA4335] absolute h-[220px] w-[176px] rounded-[20px] flex justify-center items-center overflow-hidden">
-            <img
-              className="h-full w-auto object-cover"
-              src="/assets/aboutUs3.jpg"
-              alt=""
-            />
+          <div className="box3 z-300 bg-[#EA4335] absolute h-[250px] w-[200px] rounded-[20px] flex justify-center items-center overflow-hidden">
+            <img className="h-full w-auto object-cover" src={AboutUs3} alt="" />
           </div>
-          <div className="box4 z-400 bg-[#1d1d1d] absolute h-[220px] w-[176px] rounded-[20px] flex justify-center items-center overflow-hidden">
+          <div className="box4 z-400 bg-[#1d1d1d] absolute h-[250px] w-[200px] rounded-[20px] flex justify-center items-center overflow-hidden">
             <img src="/assets/gdgLogo.png" alt="" />
           </div>
-          <div className="box5 z-500 bg-[#4285F4] absolute h-[220px] w-[176px] rounded-[20px] flex justify-center items-center overflow-hidden">
-            <img src="/assets/aboutUs1.png" alt="" />
+          <div className="box5 z-500 bg-[#4285F4] absolute h-[250px] w-[200px] rounded-[20px] flex justify-center items-center overflow-hidden">
+            <img src={AboutUs1} alt="" />
           </div>
-          <div className="box6 z-600 bg-[#FBBC04] absolute h-[220px] w-[176px] rounded-[20px] flex justify-center items-center overflow-hidden">
-            <img
-              className="h-full w-auto object-cover"
-              src="/assets/aboutUs7.jpg"
-              alt=""
-            />
+          <div className="box6 z-600 bg-[#FBBC04] absolute h-[250px] w-[200px] rounded-[20px] flex justify-center items-center overflow-hidden">
+            <img className="h-full w-auto object-cover" src={AboutUs7} alt="" />
           </div>
-          <div className="box7 z-700 bg-[#EA4335] absolute h-[220px] w-[176px] rounded-[20px] flex justify-center items-center overflow-hidden">
-            <img
-              className="h-full w-auto object-cover"
-              src="/assets/aboutUs2.jpg"
-              alt=""
-            />
+          <div className="box7 z-700 bg-[#EA4335] absolute h-[250px] w-[200px] rounded-[20px] flex justify-center items-center overflow-hidden">
+            <img className="h-full w-auto object-cover" src={AboutUs2} alt="" />
           </div>
           <div className="aboutUsTextBox z-800 text-[#272727] absolute whitespace-nowrap">
             <h1>ABOUT US</h1>
           </div>
-          <div className="textUnderAboutUs absolute text-[1.3rem] w-[400px] transform translate-x-[-400px] translate-y-[300px] text-center">
+          <div className="textUnderAboutUs absolute text-[1.3rem] w-[600px] transform translate-x-[-400px] translate-y-[300px] text-center">
             <h1>
               At GDSC MSIT,we aim to learn, teach and grow. Together, the place
               where creative minds come together to build something amazing
